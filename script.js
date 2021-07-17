086fee3c141325ae0e34b08cb61a4818cd0655f5
oemuraye <oemuraye@gmail.com>
2021-07-17 16:15:35 +0100

    made arrangements on the codes

--- script.js
+++ script.js
@@ -7,12 +7,13 @@ const navbarSearch = document.querySelector('#navbar-search')
 
 
 
-
+//hamburger toggle
 hamburger.addEventListener('click', () => {
   navbarMenu.classList.toggle('menu')
   navbarSearch.classList.remove('search-bar')
 })
 
+//Search toggle
 search.addEventListener('click', () => {
   navbarSearch.classList.toggle('search-bar')
   navbarMenu.classList.remove('menu')
